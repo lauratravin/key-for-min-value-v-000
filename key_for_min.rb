8fbdade633bfc1c2d99d4d06e7535e6918934aff
Laura Messa Travin <laura.travin@gmail.com>
2018-04-11 05:55:51 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,7 @@ def key_for_min_value(name_hash)
      nil
   else
       name_hash.collect {|key, value|
-      newarray =  value
+      newarray <<  value
        }
        name_hash.collect {|key, value|
          newarray.sort
