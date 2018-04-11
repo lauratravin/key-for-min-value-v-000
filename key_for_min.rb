2fd5e71b0662e53b8b79c4f5984fa53ba03e87f0
Laura Messa Travin <laura.travin@gmail.com>
2018-04-11 05:56:37 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,9 +10,11 @@ def key_for_min_value(name_hash)
       name_hash.collect {|key, value|
       newarray <<  value
        }
+        newarray.sort
+
        name_hash.collect {|key, value|
-         newarray.sort
-          if value ==  newarray[0]
+
+          if value == newarray[0]
           who = key
           end
        }
