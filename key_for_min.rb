a7039f6b5dcfaa73f3a928a56a9504921795887e
Laura Messa Travin <laura.travin@gmail.com>
2018-04-11 05:53:01 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,17 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  newarray = []
+   if name_hash = ""
+     nil
+  else
+      name_hash.collect {|key, value|
+      newarray =  value
+       }
+       name_hash.collect {|key, value|
+          if value ==  newarray.sort[0]
+          key
+          end  
+       }
+  end
 end
